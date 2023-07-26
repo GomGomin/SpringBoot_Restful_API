80d74ae70b624a21c7464ff2e73ba87bae877de3
GomGomin <107527861+GomGomin@users.noreply.github.com>
2023-07-26 16:14:40 +0900

    23-07-26
    컴퓨터로 하고 싶구나....

---
+++ demo/src/main/java/juniper/loca/demo/service/firstService.java
@@ -0,0 +1,9 @@
+package juniper.loca.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+public interface firstService {
+    Map<String, Object> getFirstData();
+}
