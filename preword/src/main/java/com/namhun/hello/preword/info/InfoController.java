170947a4351c29f35869a64ee835d2a05583cb27
KimNamHun <107527861+GomGomin@users.noreply.github.com>
2023-07-30 23:57:19 +0900

    23-07-30
    LogBack 연습

--- preword/src/main/java/com/namhun/hello/preword/info/InfoController.java
+++ preword/src/main/java/com/namhun/hello/preword/info/InfoController.java
@@ -3,20 +3,28 @@ package com.namhun.hello.preword.info;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.namhun.hello.preword.info.model.Project;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@Slf4j
 public class InfoController {
 
     @GetMapping("/info")
     public Project projectInfo() {
+        log.debug("/info start");
+
         Project project = new Project();
         project.projectName = "test";
         project.createadDate = new Date();
         project.author = "namhun";
+
+        String version = org.springframework.core.SpringVersion.getVersion();
+
+        log.info("retrun {}" , project.toString());
         return project;
     }
 
