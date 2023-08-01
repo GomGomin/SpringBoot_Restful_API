dcd02a428dbdc1d1db87a47e1d6d6bdf5a25ac53
KimNamHun <107527861+GomGomin@users.noreply.github.com>
2023-08-01 16:43:04 +0900

    23-08-01

--- preword/src/main/java/com/namhun/hello/preword/info/InfoController.java
+++ preword/src/main/java/com/namhun/hello/preword/info/InfoController.java
@@ -3,16 +3,26 @@ package com.namhun.hello.preword.info;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.namhun.hello.preword.info.model.City;
+import com.namhun.hello.preword.info.model.FileData;
 import com.namhun.hello.preword.info.model.InfoService;
 import com.namhun.hello.preword.info.model.Project;
+import com.namhun.hello.preword.info.storage.StorageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -20,10 +30,12 @@ import java.util.List;
 public class InfoController {
 
     private InfoService infoService;
+    private StorageService storageService;
 
     @Autowired // 스프링 4.3 버전 이상에서는 생략 가능, BeanFactory에서 InfoService를 찾는다
-    public InfoController(InfoService infoService) {
+    public InfoController(InfoService infoService, StorageService storageService) {
         this.infoService = infoService; // 생성자를 주입
+        this.storageService = storageService;
     }
 
 
@@ -125,15 +137,83 @@ public class InfoController {
 //    }
 
     @PostMapping(value="cityAdd")
-    public ResponseEntity<String> cityAdd(@RequestBody City city) {
+    public ResponseEntity<City> cityAdd(@RequestBody City city) {
         try {
             log.debug("city = {}", city.toString());
-
-            log.debug(city.getId().toString());
+            return new ResponseEntity<>(infoService.insert(city),HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-
+            log.error(e.toString());
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping(value = "cityEdit")
+    public ResponseEntity<String> cityEdit(@RequestBody City city){
+        try {
+            log.debug("city = {}", city.toString());
+            Integer updatedCnt = infoService.updateById(city);
+            return new ResponseEntity<>(String.format("%d updated", updatedCnt), HttpStatus.OK);
+        }catch (Exception e){
+            log.error(e.toString());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ResponseBody
+    @PostMapping(value = "cityDelete")
+    public ResponseEntity<String> cityDelete(@RequestParam(value="id") Integer id){
+        try{
+            log.debug("city id ={}", id);
+            Integer deleteCnt = infoService.deleteById(id);
+            return new ResponseEntity<>(String.format("%d deleted", deleteCnt), HttpStatus.OK);
+        }catch (Exception e){
+            log.error(e.toString());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(value = "uploadFile")
+    public ResponseEntity<String> uploadFile(MultipartFile file) throws IllegalStateException, IOException{
+        if(!file.isEmpty()) {
+            log.debug("file org name ={}", file.getOriginalFilename());
+            log.debug("file content type ={}",file.getContentType());
+            file.transferTo(new File(file.getOriginalFilename()));
+        }
+        return new ResponseEntity<>("",HttpStatus.OK);
+    }
+
+    @PostMapping(value = "upload")
+    public ResponseEntity<String> upload(MultipartFile file) throws IllegalStateException, IOException{
+        storageService.store(file);
         return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "download")
+    public ResponseEntity<Resource> serverFile(@RequestParam(value="filename") String filename){
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping(value="deleteAll")
+    public ResponseEntity<String> deleteAll(){
+        storageService.deleteAll();
+        return new ResponseEntity<>("",HttpStatus.OK);
+    }
+
+    @GetMapping("fileList")
+    public ResponseEntity<List<FileData>> getListFiles(){
+        List<FileData> fileInfos = storageService.loadAll().map(path -> {FileData data = new FileData();
+        String filename = path.getFileName().toString();
+        data.setFilename(filename);
+        String url = MvcUriComponentsBuilder.fromMethodName(InfoController.class, "serveFile",filename).build().toString();
+        data.setUrl(url);
+        try {
+            data.setSize(Files.size(path));
+        }catch (IOException e){
+            log.error(e.getMessage());
+        }
+        return data;
+        }).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
     }
 }
--- preword/src/main/java/com/namhun/hello/preword/info/repository/CityRepository.java
+++ preword/src/main/java/com/namhun/hello/preword/info/repository/CityRepository.java
@@ -1,11 +1,14 @@
 package com.namhun.hello.preword.info.repository;
 
 import com.namhun.hello.preword.info.model.City;
+import lombok.Generated;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -64,6 +67,39 @@ public class CityRepository {
         SqlParameterSource param = new MapSqlParameterSource("countryCode", countryCode).addValue("population", population);
 
         return namedParameterJdbcTemplate.query(sql, param, cityMapper);
+    }
+
+    public City insert(City city){
+        String sql = "INSERT INTO city (Name, CountryCode, District, Population) values (:name, :countryCode, :district, :population)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource parameterSource = new MapSqlParameterSource("name", city.getName())
+                .addValue("countryCode",city.getCountryCode())
+                .addValue("district", city.getDistrict())
+                .addValue("population", city.getPopulation());
+
+        int affectRows = namedParameterJdbcTemplate.update(sql, parameterSource, keyHolder);
+        log.debug("{} inserted, new id={}", affectRows,keyHolder.getKey());
+        city.setId(keyHolder.getKey().intValue());
+        return city;
+    }
+
+    public Integer updateById(City city){
+        String sql = "UPDATE city SET Name = :name, CountryCode = :countryCode, District = :district, Population = :population WHERE 1=1 AND id = :id";
+
+        SqlParameterSource parameterSource = new MapSqlParameterSource("id", city.getId())
+                .addValue("name", city.getName())
+                .addValue("countryCode",city.getCountryCode())
+                .addValue("district", city.getDistrict())
+                .addValue("population", city.getPopulation());
+
+        return namedParameterJdbcTemplate.update(sql, parameterSource);
+    }
+
+    public Integer deleteById(Integer id){
+        String sql = "DELETE FROM city WHERE 1=1 AND id = :id";
+
+        SqlParameterSource parameterSource = new MapSqlParameterSource("id", id);
+        return namedParameterJdbcTemplate.update(sql, parameterSource);
     }
 
 }
